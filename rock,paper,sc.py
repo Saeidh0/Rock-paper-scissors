612441dcd2e746904e6ee1c0d6f13ea7a36fa705
Saeid Anwari <malizai71011@gmail.com>
2026-09-02 00:54:57 -0400

    Define functions for each part
    Add menu to the game
    
    Add single round mode

--- rock,paper,sc.py
+++ rock,paper,sc.py
@@ -1,43 +1,89 @@
 import random
 
-user_wins = 0
-computer_wins = 0
-tie = 0
-option=['rock', 'paper', 'scissors']
+# add functions to game
+# add menu
+# add differen modes
+#side note : remove the while loop to reuse the code 
 
-while True:
-    users_input= input('Type Rock/Paper/Scissors or Q to quit.').lower()
-    
-    if users_input =='q':
-        break
-    if users_input not in ['rock', 'paper', 'scissors']:
-        print ('Invalid input. Please Type Rock/Paper/Scissors')
-        continue
-# fix the bug when there is a tie the win goes to computer.
-# add a tie tracker.
+
+
+def logic():
+    user_wins = 0
+    computer_wins = 0
+    tie = 0
+    option=['rock', 'paper', 'scissors']
+    while True:
+        users_input= input('Type Rock/Paper/Scissors\n').lower()
+        if users_input not in option:
+            print ('Invalid input.')
+        else:
+            break    
+
     computer_pick = random.choice(option)
     print('computer pick', computer_pick + '.')
+
     if users_input == computer_pick:
         tie +=1
         print("tie")
-        continue
     else:
         if users_input == 'rock' and computer_pick == 'scissors':
             print('YOU win!')
             user_wins +=1
-            continue
         elif users_input == 'scissors' and computer_pick == 'paper':
             print('YOU win!')
             user_wins +=1
-            continue
         elif users_input == 'paper' and computer_pick == 'rock':
             print('YOU win!')
             user_wins +=1
-            continue
+
         else:
             print('YOU lost')
             computer_wins +=1
+    return user_wins, computer_wins, tie
 
-print('you won', user_wins,'times')
-print('computer won', computer_wins, 'times')
-print('tie', tie , 'times')
+def single_game():
+    result = logic()
+    return(result)
+    
+
+def out_of_three():
+    pass
+
+def out_of_five():
+    pass
+
+
+# start using functions :
+def menu(single_game, out_of_three, out_of_five):
+    while True:
+        print('\n ======[ Menu ]======')
+        print('1. Single round')
+        print('2. Best out of tree')
+        print('3. Best out of five')
+        print('5. Exit')
+        print(' ====================')
+
+        menu_choice = input('Choose an option: ').strip()
+        if menu_choice == '1':
+            single_game()
+        else:
+            print('invalid input')
+
+        
+
+
+
+def main():
+    # this is to print the menu
+    menu(single_game, out_of_three, out_of_five)
+
+
+    # this is for logic
+    user_wins,computer_wins,tie = logic()
+
+    
+    print('you won', user_wins,'times')
+    print('computer won', computer_wins, 'times')
+    print('tie', tie , 'times')
+
+main()
