39b12f9b6590105f5ad519e201f9c9ab8668f3e6
Saeid Anwari <malizai71011@gmail.com>
2026-09-06 01:10:04 -0400

    GUI improvment

--- rock,paper,sc.py
+++ rock,paper,sc.py
@@ -19,8 +19,7 @@ pygame.display.set_icon(icon)
 background = pygame.image.load("background.jpg").convert()
 background = pygame.transform.scale(background,(800,500))
 
-font = pygame.font.Font(None, 40)
-big_fond = pygame.font.Font(None, 60)
+font = pygame.font.Font(None, 30)
 
 option=['rock', 'paper', 'scissors']
 
@@ -30,16 +29,16 @@ def logic(users_input):
     computer_pick = random.choice(option)
 
     if users_input == computer_pick:
-        result = 'tie'
+        result = 'Tie'
     else:
         if users_input == 'rock' and computer_pick == 'scissors':
-            result = 'You WIN'
+            result = 'You win'
         elif users_input == 'scissors' and computer_pick == 'paper':
-            result = 'You WIN'
+            result = 'You win'
         elif users_input == 'paper' and computer_pick == 'rock':
-            result = 'You WIN'
+            result = 'You win'
         else:
-            result = 'computer WIN'
+            result = 'Computer win'
     return computer_pick, result
 
 '''def round(number_of_rounds):
@@ -95,12 +94,13 @@ def main():
     user_input = ''
     computer_pick =''
     result = ''
+    user_pick = ''
 
     player_wins = 0
     computer_wins = 0
     tie =0
 
-    input_box = pygame.Rect(250,200,300,50)
+    input_box = pygame.Rect(250,110,300,40)
 
     while running:
         for event in pygame.event.get():
@@ -112,6 +112,7 @@ def main():
 
                     choice = user_input.lower().strip()
                     if choice in option:
+                        user_pick = choice
 
                         computer_pick, result = logic(choice)                
                         if result == 'player':
@@ -134,38 +135,46 @@ def main():
 #draw background
         screen_size.blit(background,(0,0))
 
-# input boc
-        pygame.draw.rect(screen_size,(255,255,255), input_box)
+# input box
+        pygame.draw.rect(screen_size,(255,255,255), input_box, border_radius=15)
+        
 # user input
         text = font.render(user_input,True, (0,0,0))
+        screen_size.blit(text,(input_box.x +10 , input_box.y +10))
 
-        screen_size.blit(text,(input_box.x +10, input_box.y + 10))
+# user input choice 
+        user_ch_rect = pygame.Rect(50,400,300,38)
+        pygame.draw.rect(screen_size,(95,158,190), user_ch_rect, border_radius=15)
+        user_text = font.render(f'You choose: {user_pick}', True, (250,250,250))
+        screen_size.blit(user_text,(user_ch_rect.x +10, user_ch_rect.y +10))
 
 #instruction 
-        instruction = font.render('Type rock, paper, or scissors and press ENTER: ', True, (255,255,255))
-        screen_size.blit (instruction,(150,150))
-# conputer chice
+        instruction = font.render('Type rock, paper, or scissors and press enter: ',
+        True, (139,69,19))
+        screen_size.blit (instruction,(180,80))
 
-        computer_text = font.render(f'computer: {computer_pick}', True, (255,255,255))
-        screen_size.blit(computer_text,(230,300))
+# computer chice
+        com_ch_rect = pygame.Rect(445,400,300,38)
+        pygame.draw.rect(screen_size,(240,128,128), com_ch_rect, border_radius =15)
+        computer_text = font.render(f'Computer choose: {computer_pick}', True, (250,250,250))
+        screen_size.blit(computer_text,(com_ch_rect.x +10, com_ch_rect.y +10))
+
+# result text background
+        result_background = pygame.Rect(250,450,300,38)
+        pygame.draw.rect(screen_size,(107,142,50), result_background,border_radius = 15)
+
 #Result
-        result_text = font.render(
-            result,
-            True,
-            (255, 255, 255)
-        )
-        screen_size.blit(result_text,(250,470))
+        result_text = font.render(result,True,(250,250,250),None)
+        text_rect = result_text.get_rect(center = result_background.center)
+        screen_size.blit(result_text,text_rect)
+
 #score
         score_text = font.render(
             f'You: {player_wins}   Computer: {computer_wins}   Ties: {tie}',
             True,
-            (255, 255, 255)
-        )
+            (255, 255, 255))
 
-        screen_size.blit(
-            score_text,
-            (230, 520)
-        )
+        screen_size.blit(score_text,(230, 520))
 
 
         pygame.display.flip()
