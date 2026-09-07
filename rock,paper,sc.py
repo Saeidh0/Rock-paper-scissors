743dfd7104adc83846843957f6096b57a329bad0
Saeid Anwari <malizai71011@gmail.com>
2026-09-06 22:41:32 -0400

    GUI improvment
    score tracking now available
    
    Add visual rock , paper, scissors

--- rock,paper,sc.py
+++ rock,paper,sc.py
@@ -20,6 +20,15 @@ pygame.display.set_icon(icon)
 background = pygame.image.load("background.jpg").convert()
 background = pygame.transform.scale(background,(800,500))
 
+#upload rock, paper, scissors image
+rock = pygame.image.load('rock.png')
+paper = pygame.image.load('paper.png')
+scissors = pygame.image.load('scissors.png')
+
+rockflip = pygame.image.load('rockflip.png')
+paperflip = pygame.image.load('paperflip.png')
+scissorsflip = pygame.image.load('scissorsflip.png')
+
 font = pygame.font.Font(None, 30)
 
 option=['rock', 'paper', 'scissors']
@@ -41,52 +50,7 @@ def logic(users_input):
         else:
             result = 'Computer win'
     return computer_pick, result
-
-'''def round(number_of_rounds):
-    user_wins = 0
-    computer_wins = 0
-    tie = 0
-
-    for i in range(number_of_rounds):
-        print(f'\nRound {i+1}:')
-        win = logic()
-
-        if win == 'player':
-            user_wins +=1
-        elif win == 'computer':
-            computer_wins +=1
-        else:
-            tie +=1
-
-    summary(user_wins,computer_wins,tie)'''
         
-'''def summary(user_wins,computer_wins, tie):
-    print('\n-----------[ summary ]------------')
-    print('Your wins:', user_wins,)
-    print('computer wins:', computer_wins)
-    print('ties:', tie )
-    print('------------------------------------')
-
-def menu ():
-    while True:
-        print('\n ======[ Menu ]======')
-        print('1. Single round')
-        print('2. Three round')
-        print('3. Five round')
-        print('4. Exit')
-        print(' ====================')
-
-        menu_choice = input('Choose an option: ').strip()
-        if menu_choice == '1':
-            logic()
-        elif menu_choice == '2':
-            round(3)
-        elif menu_choice == '3':
-            round(5)
-        elif menu_choice == '4':
-            sys.exit()
-        else:
-            print('invalid input')'''
 
 def main():
     
@@ -101,7 +65,7 @@ def main():
     computer_wins = 0
     tie =0
 
-    input_box = pygame.Rect(245,110,300,40)
+    input_box = pygame.Rect(245,100,300,40)
 
     while running:
         for event in pygame.event.get():
@@ -116,9 +80,9 @@ def main():
                         user_pick = choice
 
                         computer_pick, result = logic(choice)                
-                        if result == 'player':
+                        if result == 'You win':
                             player_wins +=1
-                        elif result == 'computer':
+                        elif result == 'Computer win':
                             computer_wins +=1
                         else:
                             tie +=1
@@ -142,10 +106,15 @@ def main():
 # user input
         text = font.render(user_input,True, (101,50,25))
         screen_size.blit(text,(input_box.x +10 , input_box.y +10))
-
+# show picture based on input
+        if user_pick == 'rock':
+            screen_size.blit(rock,(-5,170))
+        elif user_pick == 'paper':
+            screen_size.blit(paper,(-5,170))
+        elif user_pick == 'scissors':
+            screen_size.blit(scissors,(-5,170))
 # user input choice 
         user_ch_rect = pygame.Rect(50,400,300,38)
-#        pygame.draw.rect(screen_size,(95,158,190), user_ch_rect, border_radius=15)
         pygame.draw.rect(transparency,(95,158,190,180), transparency.get_rect(),border_radius=15)#for transparency
         screen_size.blit(transparency,user_ch_rect)
         user_text = font.render(f'You choose: {user_pick}', True, (250,250,250))
@@ -154,19 +123,24 @@ def main():
 #instruction 
         instruction = font.render('Type rock, paper, or scissors and press enter: ',
         True, (101,50,25))
-        screen_size.blit (instruction,(180,80))
+        screen_size.blit (instruction,(180,70))
 
 # computer chice
         com_ch_rect = pygame.Rect(445,400,300,38)
-        #pygame.draw.rect(screen_size,(240,128,128), com_ch_rect, border_radius =15)
         pygame.draw.rect(transparency,(240,128,128,180), transparency.get_rect(), border_radius = 15)
         screen_size.blit(transparency,com_ch_rect)
         computer_text = font.render(f'Computer choose: {computer_pick}', True, (250,250,250))
         screen_size.blit(computer_text,(com_ch_rect.x +10, com_ch_rect.y +10))
 
+#show picture based on computer choice
+        if computer_pick == 'rock':
+            screen_size.blit(rockflip,(420,170))
+        elif computer_pick == 'paper':
+            screen_size.blit(paperflip,(420,170))
+        elif computer_pick == 'scissors':
+            screen_size.blit(scissorsflip,(420,170))
 # result background
         result_background = pygame.Rect(250,450,300,38)
-        #pygame.draw.rect(screen_size,(107,142,50), result_background,border_radius = 15)
         pygame.draw.rect(transparency,(107,142,50,180), transparency.get_rect(), border_radius = 15)
         screen_size.blit(transparency,result_background)
 #Result text
@@ -175,19 +149,16 @@ def main():
         screen_size.blit(result_text,text_rect)
 
 #score
+
         score_text = font.render(
             f'You: {player_wins}   Computer: {computer_wins}   Ties: {tie}',
             True,
-            (255, 255, 255))
+            (101,50,25))
 
-        screen_size.blit(score_text,(230, 520))
+        screen_size.blit(score_text,(252,150))
 
 
         pygame.display.flip()
-
-
-
-    #menu()
 
     pygame.quit()
     sys.exit()
